35a6e6926e101410b1731d30497edba28fc228b0
shulmah <80043404+shulmah@users.noreply.github.com>
2021-11-06 21:23:54 +0300

    Update radio-heading.py
    fix error

--- radio-heading.py
+++ radio-heading.py
@@ -7,7 +7,7 @@ response = requests.get("http://www.radio.ru/")
 page = response.text
 
 soup = BeautifulSoup(page, 'html.parser')
-                                              # "span.news-d" "span.newa-h" < in radio
+                                              # "span.news-d" "span.news-h" < in radio
 headings = map(lambda e: e.text, soup.select("span.news-t")) #"h3.entry-title a span" <in xakep
 for h in headings:
   print(h)
