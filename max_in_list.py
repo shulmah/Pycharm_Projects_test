64db414324121f0369527e7018102684eb2cd301
shulmah <80043404+shulmah@users.noreply.github.com>
2022-01-26 14:25:24 +0300

    Update max_in_list.py
    add comments...

--- max_in_list.py
+++ max_in_list.py
@@ -1,4 +1,4 @@
-def max2(x,y):
+def max2(x,y):        # максимум из 2-х чисел
 	return x if x>y else y
 
 spis= [8, 5, 9, 91,43,17,5,88,28,52,99] 
@@ -7,11 +7,6 @@ mem = max2(spis[0], spis[1])
 for i in range(2,len(spis)):
 	if i >= len(spis):
 		break
-	mem =max2(spis[i], mem)
-	#i+=1
-	
-	#return mem
-print('max =',mem)
+	mem = max2(spis[i], mem)
 
-print()
-	
+print('max =',mem) # печать наибольшего из списка
